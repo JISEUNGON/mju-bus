7bc31d360ddf9eda99c02adca895190f9d3818f6
BEMELON <hgdkim2@naver.com>
2022-08-05 19:37:04 +0900

    Update Bus & Station Controller

---
+++ server/server/src/main/java/com/mjubus/server/controller/BusController.java
@@ -0,0 +1,50 @@
+package com.mjubus.server.controller;
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/bus")
+@Api(tags = {"버스 정보 조회 API"})
+public class BusController {
+
+    @GetMapping("/{busID}")
+    @ApiOperation(value = "버스에 대한 정보를 조회한다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "정상 응답"),
+    })
+    @ResponseBody
+    public String info(@PathVariable(value = "busID") String id) {
+        return "{\"sid\":\"ee1d5094-9a6b-4c04-95cb-b4d1f29ba303,\",\"name\":\"명지대역,\",\"charge\":\"0,\",\"type\":\"1\"}";
+    }
+
+
+
+    @GetMapping("/{busID}/status")
+    @ApiOperation(value = "버스 운행 여부를 조회한다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "정상 응답"),
+    })
+    @ResponseBody
+    public String status(@PathVariable(value = "busID") String id) {
+        return "{\"sid\":\"ee1d5094-9a6b-4c04-95cb-b4d1f29ba303,\",\"name\":\"명지대역,\",\"status\":\"1\"}";
+    }
+
+    @GetMapping("/{busID}/route")
+    @ApiOperation(value = "버스가 경유하는 정류장을 조회한다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "정상 응답"),
+    })
+    @ResponseBody
+    public String stationList(@PathVariable(value = "busID") String id) {
+        return "{\"sid\":\"ee1d5094-9a6b-4c04-95cb-b4d1f29ba303,\",\"name\":\"명지대역,\",\"routes\":[{\"sid\":\"cafe5065-8db9-450a-b4d1-83dc9c5fbbf9\",\"name\":\"명지대버스관리사무소\",\"order\":\"1\"},{\"sid\":\"a5bccfc2-f93c-456d-8164-db9dfacb1249,\",\"name\":\"상공회의소,\",\"order\":\"2\"},{\"sid\":\"8bb7245b-3ee2-4d87-b5e1-1aab2ab02850,\",\"name\":\"진입로(명지대역행),\",\"order\":\"3\"},{\"sid\":\"f05a3600-cbdf-4c38-ab74-146b2dac9e9d,\",\"name\":\"명지대역,\",\"order\":\"4\"},{\"sid\":\"e0f62712-cd5d-456b-ac3a-c92337f3384c,\",\"name\":\"진입로(명지대행),\",\"order\":\"5\"},{\"sid\":\"008d03ca-9409-4546-9ac3-ea4b038abb6a,\",\"name\":\"이마트,\",\"order\":\"6\"},{\"sid\":\"19cb0c7f-6993-4507-8d3c-539eb34effb3,\",\"name\":\"명진당,\",\"order\":\"7\"},{\"sid\":\"25963aa3-29c5-4372-9893-921e35428a58,\",\"name\":\"제3공학관,\",\"order\":\"8\"}]}";
+    }
+}
