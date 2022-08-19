9ec49e3bb60ae825a63cfeca3a4619f3cc25ab60
BEMELON <hgdkim2@naver.com>
2022-08-19 19:55:36 +0900

    Bus 운행 status가 제대로 출력되지 않는 버그 수정

--- server/server/src/main/java/com/mjubus/server/repository/BusTimeTableDetailRepository.java
+++ server/server/src/main/java/com/mjubus/server/repository/BusTimeTableDetailRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BusTimeTableDetailRepository extends JpaRepository<BusTimeTableDetail, Long> {
-    Optional<List<BusTimeTableDetail>> findBusTimeTableDetailsByBusTimeTableInfo_Id(Long busTimeTableInfo_id);
+    Optional<List<BusTimeTableDetail>> findBusTimeTableDetailsByBusTimeTableInfo_IdOrderByDepart(Long busTimeTableInfo_id);
 }
