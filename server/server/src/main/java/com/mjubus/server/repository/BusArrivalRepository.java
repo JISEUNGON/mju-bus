ee8c47fdfb1410b625411e9f725747918c3d88f0
BEMELON <hgdkim2@naver.com>
2022-09-01 21:32:14 +0900

    - 버그 fix

--- server/server/src/main/java/com/mjubus/server/repository/BusArrivalRepository.java
+++ server/server/src/main/java/com/mjubus/server/repository/BusArrivalRepository.java
@@ -14,7 +14,7 @@ public interface BusArrivalRepository extends JpaRepository<BusArrival, String> 
     @Query(value = "select * from bus_arrival where station_id = ?1 and created_at >= ?2", nativeQuery = true)
     Optional<List<BusArrival>> findAllByStationIdAndDate(Long stationId, LocalDateTime localDateTime);
 
-    @Query(value = "SELECT ba.station_id, ba.bus_id, MIN(ba.expected_at), ba.created_at" +
+    @Query(value = "SELECT ba.station_id as stationId, ba.bus_id as busId, MIN(ba.expected_at) as expectedAt, ba.created_at as createdAt" +
             "       FROM bus_arrival ba" +
             "       INNER JOIN (" +
             "           SELECT bus_id, station_id, MAX(created_at) as lastest_created_at" +
@@ -22,7 +22,8 @@ public interface BusArrivalRepository extends JpaRepository<BusArrival, String> 
             "           WHERE station_id = :station_id " +
             "           GROUP BY bus_id " +
             ") temp ON temp.bus_id = ba.bus_id AND temp.station_id = ba.station_id AND temp.lastest_created_at = ba.created_at " +
-            "GROUP BY ba.station_id, ba.bus_id, ba.created_at;", nativeQuery = true)
+            "GROUP BY ba.station_id, ba.bus_id, ba.created_at " +
+            "HAVING expectedAt > NOW();", nativeQuery = true)
     List<BusArrivalDto> findBusArrivalByStationId(@Param("station_id")Long stationId);
 
 
