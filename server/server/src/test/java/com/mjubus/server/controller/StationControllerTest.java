aa57c986ff69e312efd1edb3ca744fcf526e0bc4
BEMELON <hgdkim2@naver.com>
2022-09-09 01:14:50 +0900

    테스트코드 수

--- server/server/src/test/java/com/mjubus/server/controller/StationControllerTest.java
+++ server/server/src/test/java/com/mjubus/server/controller/StationControllerTest.java
@@ -1,18 +1,12 @@
 package com.mjubus.server.controller;
 
 import com.mjubus.server.domain.Station;
-import com.mjubus.server.dto.busArrival.BusArrivalResponse;
-import com.mjubus.server.util.DateHandler;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -31,19 +25,5 @@ class StationControllerTest {
 
     System.out.println(result.getName() + " " + result.getLatitude() + " " + result.getLatitude());
   }
-
-  @Test
-  void 도착_버스틀_조회() {
-    BusArrivalResponse result = stationController.busRemains(3L);
-
-    System.out.println("=============");
-
-    busCalendarController.setDate("2022-09-01 09:00");
-
-    for(int i = 0; i < result.getBusList().size(); i++) {
-      System.out.println(result.getBusList().size());
-    }
-
-    System.out.println("=============");
-  }
+  
 }
--- server/server/src/test/java/com/mjubus/server/service/route/RouteServiceTest.java
+++ server/server/src/test/java/com/mjubus/server/service/route/RouteServiceTest.java
@@ -4,10 +4,8 @@ import com.mjubus.server.controller.BusCalendarController;
 import com.mjubus.server.domain.Route;
 import com.mjubus.server.domain.RouteInfo;
 import com.mjubus.server.dto.StationDTO;
-import com.mjubus.server.repository.RouteInfoRepository;
 import com.mjubus.server.service.bus.BusService;
-import com.mjubus.server.service.mjuCalendar.BusCalendarService;
-import org.assertj.core.api.Assertions;
+import com.mjubus.server.util.DateHandler;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,8 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -45,6 +42,7 @@ class RouteServiceTest {
   @Test
   void findStationsByRouteInfo() {
     busCalendarController.setDate("2022-09-01 09:09");
+    DateHandler.reset();
 
   }
 
@@ -63,6 +61,7 @@ class RouteServiceTest {
   void findRouteInfoByBus() {
     LocalDateTime today = busCalendarController.setDate("2022-11-01 09:00");
     RouteInfo result = routeService.findRouteInfoByBus(busService.findBusByBusId(20L));
+    DateHandler.reset();
 
     assertThat(result.getName()).isEqualTo("시내 노선도");
   }
