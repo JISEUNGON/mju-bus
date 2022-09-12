8eb82dbd59e4c83a0d775819a5e96cc37028fe17
BEMELON <hgdkim2@naver.com>
2022-09-13 02:40:41 +0900

    정류장 위도/경도 수정

--- server/tools/getRouteDetail.py
+++ server/tools/getRouteDetail.py
@@ -16,17 +16,18 @@ def main():
     stationInfo = {
         "명지대": [37.224284, 127.187286],
         "상공회의소": [37.23068, 127.188246],
-        "진입로": [37.233992, 127.188829],
+        "진입로": [37.233993,127.188858],
         "명지대역": [37.23400397430913, 127.18860239872971],
-        "진입로(명지대방향)": [37.233999900000015, 127.18861349999999],
-        "이마트": [37.230369, 127.187997],
+        "진입로(명지대방향)": [37.23400397430913,127.18860239872971],
+        "이마트": [37.230473,127.187983],
         "명진당": [37.222184, 127.188953],
-        "제3공학관": [37.219509, 127.182992],
-        "동부경찰서": [37.234764, 127.198835],
-        "용인시장": [37.23542, 127.206684],
-        "중앙공영주차장": [37.233916, 127.208927],
-        "제1공학관": [37.222711, 127.186784],
-        "시외버스 정류장" : [37.224183,127.188367]
+        "제3공학관": [37.219509,127.182992],
+        "동부경찰서": [37.2347414,127.1986742],
+        "용인시장": [37.23537,127.2064454],
+        "중앙공영주차장": [37.2336199,127.2088728],
+        "제1공학관": [37.222711,127.186784],
+        "시외버스 정류장" : [37.224183,127.188367],
+        "기흥역" : [37.274635,127.115793]
     }
 
     NAVER_endPoint = "https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving"
@@ -34,15 +35,15 @@ def main():
     NAVER_DEST = "&goal="
     NAVER_OPTION = "&option=trafast"
 
-    # stations = ["명지대", "상공회의소", "진입로", "명지대역", "진입로(명지대방향)", "이마트", "명지대"]
-    stations = ["진입로(명지대방향)", "시외버스 정류장"]
-
+    stations = ["중앙공영주차장", "진입로(명지대방향)"]
     for i in range(0, len(stations) - 1):
         url = NAVER_endPoint + NAVER_SRC + str(stationInfo[stations[i]][1]) + "," + str(
             stationInfo[stations[i]][0]) + NAVER_DEST + str(stationInfo[stations[i + 1]][1]) + "," + str(
             stationInfo[stations[i + 1]][0]) + NAVER_OPTION
         pathlist = getPath(url)
         for i, path in enumerate(pathlist):
-            print(f"INSERT INTO path_detail (id, path_info_id, latitude, longitude, path_order) VALUES (null, 19, {path[1]}, {path[0]}, {i + 1});")
+            print(f"INSERT INTO path_detail (id, path_info_id, latitude, longitude, path_order) VALUES (null, 18, {path[1]}, {path[0]}, {i + 1});")
+            # print(path[1], path[0])
+
 
 main()
