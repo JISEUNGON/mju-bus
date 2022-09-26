0fe05fbf56a59ebb84d37788a83f7b35e98ab991
sungxsoo <njs783075@gmail.com>
2022-09-26 20:46:35 +0900

    안드로이드 설정

--- app/App.js
+++ app/App.js
@@ -3,12 +3,13 @@ import { NavigationContainer } from "@react-navigation/native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "styled-components/native";
 import { useColorScheme } from "react-native";
+import CodePush from "react-native-code-push";
 import Root from "./navigation/Root";
 import { darkTheme, lightTheme } from "./styled";
 
 const queryClient = new QueryClient();
 
-export default function App() {
+function App() {
   const isDark = useColorScheme() === "dark";
   return (
     <QueryClientProvider client={queryClient}>
@@ -20,3 +21,15 @@ export default function App() {
     </QueryClientProvider>
   );
 }
+
+const codePushOptions = {
+  checkFrequency: CodePush.CheckFrequency.ON_APP_START,
+  updateDialog: {
+    title: "...",
+    optionalUpdateMessage: "...",
+    optionalInstallButtonLabel: "업데이트",
+    optionalIgnoreButtonLabel: "아니요.",
+  },
+  installMode: CodePush.InstallMode.IMMEDIATE,
+};
+export default CodePush(codePushOptions)(App);
