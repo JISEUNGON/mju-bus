2727496f4b6ebb4506a7148d8cbd6c8032b38163
terria1020 <jaehan1346@gmail.com>
2023-01-16 19:09:17 +0900

    feat: 컨트롤러로 들어오는 요청에 대한 request 출력 로그 기능 추가
    컨트롤러로 들어오는 요청에 대해서 HttpMethod와 URI 로그를 추가하는 Aspect 추가

---
+++ server/server/src/main/java/com/mjubus/server/aspect/ControllerLoggerAspect.java
@@ -0,0 +1,23 @@
+package com.mjubus.server.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Component
+@Aspect
+public class ControllerLoggerAspect {
+
+    @Before("execution(* com.mjubus.server.controller..*(..))")
+    public void beforeController(JoinPoint joinPoint) {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        log.info("[CONTROLLER] | HttpMethod: /{} URI:{}", request.getMethod(), request.getRequestURI());
+    }
+}
