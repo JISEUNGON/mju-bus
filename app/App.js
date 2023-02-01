5f61294e99e3caa02f23c5206bd80432706b65c1
sunxsoo <njs783075@gmail.com>
2023-02-01 19:13:58 +0900

    feat: logScreenView 설정

--- app/App.js
+++ app/App.js
@@ -1,5 +1,9 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { React, useRef } from "react";
+import analytics from "@react-native-firebase/analytics";
+import {
+  NavigationContainer,
+  useNavigationContainerRef,
+} from "@react-navigation/native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "styled-components/native";
 import { useColorScheme } from "react-native";
@@ -12,6 +16,8 @@ import SyncProgressView from "./screens/SyncProgressView";
 const queryClient = new QueryClient();
 
 function App() {
+  const navigationRef = useNavigationContainerRef();
+  const routeNameRef = useRef();
   const isDark = useColorScheme() === "dark";
   const [isUpdating, syncProgress] = useCodePush();
   return (
@@ -20,7 +26,31 @@ function App() {
         {isUpdating ? (
           <SyncProgressView syncProgress={syncProgress} />
         ) : (
-          <NavigationContainer>
+          <NavigationContainer
+            ref={navigationRef}
+            onReady={() => {
+              routeNameRef.current =
+                navigationRef.current.getCurrentRoute()?.name;
+            }}
+            onStateChange={() => {
+              const previousScreenName = routeNameRef.current;
+
+              const currentRoute = navigationRef.current.getCurrentRoute();
+
+              const currentScreenName = `${currentRoute?.name}_${Object.values(
+                currentRoute?.params || {},
+              ).join("/")}`;
+
+              if (currentRoute && previousScreenName !== currentScreenName) {
+                analytics().logScreenView({
+                  screen_name: currentScreenName,
+                  screen_class: currentRoute.name,
+                });
+              }
+              // Save the current route name for later comparision
+              routeNameRef.current = currentScreenName;
+            }}
+          >
             <Root />
           </NavigationContainer>
         )}
