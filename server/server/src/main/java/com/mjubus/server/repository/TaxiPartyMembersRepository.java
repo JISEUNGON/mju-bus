13657c88e0b9159289b21166c1ad572e333fe602
BEMELON <hgdkim2@naver.com>
2023-02-10 01:23:51 +0900

    chore: Test를 위한 테스트 yml 파일 설정

--- server/server/src/main/java/com/mjubus/server/repository/TaxiPartyMembersRepository.java
+++ server/server/src/main/java/com/mjubus/server/repository/TaxiPartyMembersRepository.java
@@ -4,10 +4,13 @@ import com.mjubus.server.domain.TaxiPartyMembers;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 
 
+@Repository
 public interface TaxiPartyMembersRepository extends JpaRepository<TaxiPartyMembers, Long> {
 
     List<TaxiPartyMembers> findTaxiPartyMembersByTaxiParty_Id(Long taxiParty_id);
