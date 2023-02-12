114e6d49a236bd546f33abc5007e29fb9eb9be47
BEMELON <hgdkim2@naver.com>
2023-02-12 16:03:46 +0900

    feat: Secuirty Config에 대해서 일시적인 검증 해제

--- server/server/src/main/java/com/mjubus/server/config/SecurityConfig.java
+++ server/server/src/main/java/com/mjubus/server/config/SecurityConfig.java
@@ -1,17 +1,13 @@
 package com.mjubus.server.config;
 
-import com.mjubus.server.security.JwtMemberFilter;
 import com.mjubus.server.service.member.MemberService;
 import com.mjubus.server.service.taxiPartyMembers.TaxiPartyMembersService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity  // 해당 애노테이션을 붙인 필터(현재 클래스)를 스프링 필터체인에 등록.
@@ -34,16 +30,17 @@ public class SecurityConfig {
         return httpSecurity
                 .httpBasic().disable()
                 .csrf().disable()
-                .cors().and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .addFilterBefore(new JwtMemberFilter(memberService, secretKey), UsernamePasswordAuthenticationFilter.class)
-                .authorizeRequests()
-                .antMatchers("/login/**").permitAll()
-                .antMatchers("/taxi/**/delete").hasAnyAuthority("ROLE_ADMIN", "GROUP_ADMIN")
-                .antMatchers("/taxi/**/chatting/**").hasAnyAuthority("ROLE_ADMIN", "GROUP_ADMIN", "GROUP_MEMBER")
-                .antMatchers(HttpMethod.DELETE, "/taxi/**").hasAnyAuthority("ROLE_ADMIN", "GROUP_ADMIN", "GROUP_MEMBER")
-                .antMatchers("/member/**", "/taxi/**").hasAnyAuthority("ROLE_MEMBER", "ROLE_ADMIN", "GROUP_ADMIN", "GROUP_MEMBER")
-                .and().build();
+                .cors().disable()
+                .authorizeRequests().antMatchers("**").permitAll().and().build();
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+//                .addFilterBefore(new JwtMemberFilter(memberService, secretKey), UsernamePasswordAuthenticationFilter.class)
+//                .authorizeRequests()
+//                .antMatchers("/login/**").permitAll()
+//                .antMatchers("/taxi/**/delete").hasAnyAuthority("ROLE_ADMIN", "GROUP_ADMIN")
+//                .antMatchers("/taxi/**/chatting/**").hasAnyAuthority("ROLE_ADMIN", "GROUP_ADMIN", "GROUP_MEMBER")
+//                .antMatchers(HttpMethod.DELETE, "/taxi/**").hasAnyAuthority("ROLE_ADMIN", "GROUP_ADMIN", "GROUP_MEMBER")
+//                .antMatchers("/member/**", "/taxi/**").hasAnyAuthority("ROLE_MEMBER", "ROLE_ADMIN", "GROUP_ADMIN", "GROUP_MEMBER")
+//                .and().build();
     }
 
 }
