07bdd6001cbbf28259166d82a4c2f6d6d69ce8d0
BEMELON <hgdkim2@naver.com>
2023-02-13 18:34:26 +0900

    fix: SwaggerConfig가 어떤 사유로 사라진 건 수정

--- server/server/src/main/java/com/mjubus/server/config/SwaggerConfig.java
+++ server/server/src/main/java/com/mjubus/server/config/SwaggerConfig.java
@@ -5,21 +5,28 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+    private static final String REFERENCE = "Bearer ";
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
                 .apiInfo(SwaggerInfo()).select()
                 .apis(RequestHandlerSelectors.basePackage("com.mjubus.server.controller"))
                 .paths(PathSelectors.any())
                 .build()
-                .useDefaultResponseMessages(false);
+                .useDefaultResponseMessages(false)
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(bearerAuthSecurityScheme()));
     }
 
     private ApiInfo SwaggerInfo() {
@@ -27,6 +34,27 @@ public class SwaggerConfig {
                 .title("mju-bus Swagger")
                 .description("메인 API Swagger 문서")
                 .version("1.0")
+                .build();
+    }
+
+    private SecurityContext securityContext() {
+        return springfox.documentation
+                .spi.service.contexts
+                .SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .operationSelector(operationContext -> true)
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = new AuthorizationScope("global", "accessEverything");
+        return List.of(new SecurityReference(REFERENCE, authorizationScopes));
+    }
+
+    private HttpAuthenticationScheme bearerAuthSecurityScheme() {
+        return HttpAuthenticationScheme.JWT_BEARER_BUILDER
+                .name(REFERENCE)
                 .build();
     }
 }
