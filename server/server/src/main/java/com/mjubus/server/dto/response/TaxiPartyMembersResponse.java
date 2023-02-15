c8282400a421779129f34598e613ccd0490177a2
terria1020 <jaehan1346@gmail.com>
2023-02-15 15:32:17 +0900

    refactor: 택시 파티의 그룹 참여 인원 정보 조회 결과에 파티장을 추가 등
    택시 파티의 그룹 참여 인원 정보 조회 결과에 파티장을 추가
    요청 결과에서 일부 불필요한 내용 삭제

--- server/server/src/main/java/com/mjubus/server/dto/response/TaxiPartyMembersResponse.java
+++ server/server/src/main/java/com/mjubus/server/dto/response/TaxiPartyMembersResponse.java
@@ -11,26 +11,31 @@ import lombok.Getter;
 @Builder
 public class TaxiPartyMembersResponse {
 
-    @ApiModelProperty(example = "고유 식별 ID")
+    @ApiModelProperty(example = "멤버 ID")
     private Long id;
 
-    @ApiModelProperty(example = "파티 멤버")
-    private Member member;
+//    @ApiModelProperty(example = "파티 멤버")
+//    private Member member;
 
 //    @ApiModelProperty(example = "파티 ID")
 //    private TaxiParty taxiPartyId;
 
-    public static TaxiPartyMembersResponse of (Long id, Member member){
-        return TaxiPartyMembersResponse.builder()
-                .id(id)
-                .member(member)
-                .build();
-    }
+//    public static TaxiPartyMembersResponse of (Long id, Member member){
+//        return TaxiPartyMembersResponse.builder()
+//                .id(id)
+//                .member(member)
+//                .build();
+//    }
+//
+//    public static TaxiPartyMembersResponse of (TaxiPartyMembers result){
+//        return TaxiPartyMembersResponse.builder()
+//                .id(result.getId())
+//                .member(result.getMember())
+//                .build();
+//    }
+    @ApiModelProperty(example = "멤버 이름")
+    private String name;
 
-    public static TaxiPartyMembersResponse of (TaxiPartyMembers result){
-        return TaxiPartyMembersResponse.builder()
-                .id(result.getId())
-                .member(result.getMember())
-                .build();
-    }
+    @ApiModelProperty(example = "프로필 이미지 url")
+    private String profileImageUrl;
 }
