71d8f5ac52d23f8f7e455b6d014b9c9ce1a54b4c
BEMELON <hgdkim2@naver.com>
2023-02-15 22:55:44 +0900

    chore: 빠졌던 설정파일 추가

--- app/components/Login/GoogleLogin.js
+++ app/components/Login/GoogleLogin.js
@@ -38,19 +38,6 @@ const GoogleLogin = () => {
                 navigation.navigate("StudentAuth");
             } catch (error) {
                 console.log(error);
-                // if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-                //     // user cancelled the login flow
-                //     console.log('user cancelled the login flow');
-                // } else if (error.code === statusCodes.IN_PROGRESS) {
-                //     // operation (e.g. sign in) is in progress already
-                //     console.log('operation (e.g. sign in) is in progress already');
-                // } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-                //     // play services not available or outdated
-                //     console.log('play services not available or outdated');
-                // } else {
-                //     // some other error happened
-                //     console.log('some other error happened');
-                // }
             }
         };
         await signIn();
