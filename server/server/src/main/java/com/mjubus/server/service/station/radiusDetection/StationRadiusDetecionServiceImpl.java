341444c23855e3a01e14997ec91d6817855735f5
terria1020 <jaehan1346@gmail.com>
2023-02-18 22:49:06 +0900

    feat: 정류장(장소)와 위도경도의 거리를 계산하여 반경을 감지해 정류장(장소)의 이름을 반환하는 서비스 구현

--- server/server/src/main/java/com/mjubus/server/service/station/radiusDetection/StationRadiusDetecionServiceImpl.java
+++ server/server/src/main/java/com/mjubus/server/service/station/radiusDetection/StationRadiusDetecionServiceImpl.java
@@ -4,11 +4,15 @@ import com.mjubus.server.domain.StationLocation;
 import com.mjubus.server.dto.request.StationRadiusDetectedNameRequest;
 import com.mjubus.server.dto.response.StationRadiusDetectedNameResponse;
 import com.mjubus.server.repository.StationLocationRepository;
+import com.mjubus.server.util.HaversineUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -24,9 +28,21 @@ public class StationRadiusDetecionServiceImpl implements StationRadiusDetectionS
     @Override
     public StationRadiusDetectedNameResponse detectAndGetName(StationRadiusDetectedNameRequest request) {
         List<StationLocation> all = stationLocationRepository.findAll();
-        all.forEach(stationLocation -> {
-            log.info("lat: {}, lon: {}", stationLocation.getLatitude(), stationLocation.getLongitude());
-        });
-        return null;
+        Optional<StationLocation> matchs = all.stream().
+                filter(stationLocation -> {
+                    return HaversineUtil.distance(request.getLatitude(),
+                            request.getLongitude(),
+                            stationLocation.getLatitude(),
+                            stationLocation.getLongitude()
+                    ) < stationLocation.getRange();
+                })
+                .sorted(Comparator.comparing(StationLocation::getPriority))
+                .findFirst();
+
+        StationLocation matchLocation = matchs.orElseThrow(() -> new IllegalStateException("없는 장소"));
+
+        return StationRadiusDetectedNameResponse.builder()
+                .name(matchLocation.getName())
+                .build();
     }
 }
